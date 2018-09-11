911552571b297fb995377cd192b2dcabbed05f44
Frank <frank@iMac3.local>
2018-09-11 11:08:38 -0400

    draw four octagons, try to erase the line between them.

---
+++ assignment one.py
@@ -0,0 +1,24 @@
+import turtle
+
+
+for x in range(8):
+    turtle.forward(100)
+    turtle.left(45)
+
+turtle.position(-300, -300)
+
+for x in range(8):
+    turtle.forward(100)
+    turtle.left(45)
+
+turtle.position(200,200)
+
+for x in range(8):
+    turtle.forward(100)
+    turtle.left(45)
+
+turtle.position(200,-200)
+
+for x in range(8):
+    turtle.forward(100)
+    turtle.left(45)
