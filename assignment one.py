89e30da01ab2b65025995ad856d2a1a6fbb76f0d
Frank <frank@iMac3.local>
2018-09-13 12:07:35 -0400

    I have tried this assignment for many times, and finally succeed at last. In addition, this is my first program that I have everdone.

--- assignment one.py
+++ assignment one.py
@@ -1,29 +1,58 @@
+# Frank Zhang
+# 9/13/18
+# assignment one.py
+# I have tried this assignment for many times, and finally succeed at last. In addition, this is my first program that I have everdone.
+
+
+
 import turtle
 
+turtle.color("black","red")
+turtle.begin_fill()
 
 for x in range(8):
     turtle.forward(100)
     turtle.left(45)
 
-fillcolor('#00CCFF')
+turtle.end_fill()
 
-turtle.setx(-300)
-turtle.sety(-300)
+turtle.penup()
+turtle.goto(-300,-300)
+turtle.pendown()
 
-for x in range(8):
-    turtle.forward(100)
-    turtle.left(45)
-
-turtle.setx(200)
-turtle.sety(200)
+turtle.color("black","green")
+turtle.begin_fill()
 
 for x in range(8):
     turtle.forward(100)
     turtle.left(45)
 
-turtle.setx(200)
-turtle.sety(-200)
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(200,200)
+turtle.pendown()
+
+turtle.color("black","yellow")
+turtle.begin_fill()
 
 for x in range(8):
     turtle.forward(100)
     turtle.left(45)
+
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(200,-200)
+turtle.pendown()
+
+turtle.color("black","pink")
+turtle.begin_fill()
+
+for x in range(8):
+    turtle.forward(100)
+    turtle.left(45)
+
+turtle.end_fill()
+
+turtle.exitonclick()
